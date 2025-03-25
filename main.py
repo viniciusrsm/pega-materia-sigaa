0b54e8b0e77948f72d17bec0e84d8b4b7c9114f8
unknown <viniciusregadassm@gmail.com>
2025-03-25 14:52:27 -0300

    feat: enroll check eternal loop

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import os
-import time
 
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
 class SigaaEnroller:
-    def __init__(self, codesClasses):
+    def __init__(self, coursesCodes: list[(str, str)]):
         load_dotenv()
-        self.codesClasses = codesClasses
+        self.coursesCodes = coursesCodes
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         
@@ -20,21 +18,38 @@ class SigaaEnroller:
             self.driver.get("https://sigaa.unb.br/sigaa/paginaInicial.do")
             self.login()
             self.navigate_to_enrollment()
-            self.enroll()
-            self.confirm()
+
+            c = 0
+            while True:
+                code = coursesCodes[c][0]
+                classNum = coursesCodes[c][1]
+
+                enrollSuccess = self.enroll(code, classNum)
+                if not (enrollSuccess):
+                    continue
+
+                confirmSuccess = self.confirm(c, code)
+                if not (confirmSuccess):
+                    break
+                
+                if (len(self.coursesCodes)): c = (c+1) % len(self.coursesCodes)
+                else: break
+
         except Exception as e:
             print(f"Error: {e}")
             
     def login(self):
+        self.driver.implicitly_wait(10)
         usernameInput = self.driver.find_element(By.ID, "username")
         passwordInput = self.driver.find_element(By.ID, "password")
         submitBtn = self.driver.find_element(By.NAME, "submit")
+
         usernameInput.send_keys(os.getenv('USER'))
         passwordInput.send_keys(os.getenv('PASSWORD'))
         submitBtn.click()
-        self.driver.implicitly_wait(10)
         
     def navigate_to_enrollment(self):
+        self.driver.implicitly_wait(10)
         cookiesButton = self.driver.find_element(By.CSS_SELECTOR, "#sigaa-cookie-consent > button")
         menu = self.driver.find_element(By.CSS_SELECTOR, "td.ThemeOfficeMainItem:nth-child(1)")
         subMenu = self.driver.find_element(By.CSS_SELECTOR, "tr.ThemeOfficeMenuItem:nth-child(13) > td:nth-child(2)")
@@ -44,25 +59,34 @@ class SigaaEnroller:
         menu.click()
         subMenu.click()
         enrollPageBtn.click()
-        self.driver.implicitly_wait(10)
         
-    def enroll(self):
+        
+    def enroll(self, code, classNumber):
+        self.driver.implicitly_wait(10)
         codeInput = self.driver.find_element(By.ID, "form:txtCodigo")
-        codeInput.send_keys(self.codesClasses[0][0])
+        codeInput.send_keys(code)
         codeInput.send_keys(Keys.RETURN)
 
         self.driver.implicitly_wait(10)
-        classesTable = self.driver.find_element(By.ID, "lista-turmas-extra")
+        self.driver.find_element(By.ID, "form")
+
+        try:
+            classesTable = self.driver.find_element(By.ID, "lista-turmas-extra")
+        except:
+            return False
+        
         classes = classesTable.find_elements(By.TAG_NAME, "tr")
         for cla in classes:
-            if (self.codesClasses[0][1] in cla.text):
+            if (classNumber in cla.text):
                 btn = cla.find_elements(By.TAG_NAME, "a")[1]
                 btn.click()
                 break
+
+        return True
+
+    def confirm(self, arrIndex, code):
         self.driver.implicitly_wait(10)
-        
-    def confirm(self):
-        confirmTable = self.driver.find_element(By.CSS_SELECTOR, ".subFormulario > tbody:nth-child(2)")
+        confirmTable = self.driver.find_element(By.XPATH, '//*[@id="j_id_jsp_334536566_1"]/table[2]/tbody/tr[3]/td/div/div/table')
         fields = confirmTable.find_elements(By.TAG_NAME, "tr")
         for field in fields:
             inputValue = ''
@@ -76,19 +100,34 @@ class SigaaEnroller:
             fieldInput = field.find_element(By.TAG_NAME, "input")
             fieldInput.send_keys(inputValue)
         
-        confirmBtn = self.driver.find_element(By.CSS_SELECTOR, "#j_id_jsp_334536566_1\:btnConfirmar")
+        confirmBtn = self.driver.find_element(By.XPATH, '//*[@id="j_id_jsp_334536566_1:btnConfirmar"]')
         confirmBtn.click()
         alert = self.driver.switch_to.alert
-        alert.accept()  
+        alert.accept()
+
+        try:
+            self.driver.implicitly_wait(30)
+            self.driver.find_element(By.CLASS_NAME, 'subFormulario')
+        except:
+            print("Suas informações pessoais estão incorretas!")
+            return False
+        finally:
+            self.coursesCodes.pop(arrIndex)
+            print(f"Cadastrado na disciplina {code}")
+
+            returnBtn = self.driver.find_element(By.XPATH, '//*[@id="j_id_jsp_334536566_1:btnRealizarNovaMatricula"]')
+            returnBtn.click()
+
+            return True
 
 if __name__ == "__main__":
-    codesClasses = []
-    classesCount = input("Quantas disciplinas deseja se matricular? \n")
-    for i in range(int(classesCount)):
+    coursesCodes = []
+    coursesCount = input("Quantas disciplinas deseja se matricular? \n")
+    for i in range(int(coursesCount)):
         code = input(f"Digite o código da disciplina {i+1}: \n").replace(' ', '')
         classNumber = input("Digite o número da turma dessa disciplina: \n").lstrip('0').replace(' ', '')
         if (int(classNumber) < 10): classNumber = '0' + classNumber
-        codesClasses.append((code, f'Turma {classNumber}'))
+        coursesCodes.append((code, f'Turma {classNumber}'))
     
-    enroller = SigaaEnroller(codesClasses)
+    enroller = SigaaEnroller(coursesCodes)
     enroller.run()
